e9d98623b5ca288e0e6de30075d5017e756fde82
WannabeDagobert <maximnemeth@gmail.com>
2024-02-19 16:31:47 +0100

    added object

---
+++ DodgeTheMonkeyss/Assets/scripts/GameManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject block;
+    public float maxX;
+    public Transform spawnPoint;
+    public float spawnRate;
+
+
+
+    bool gameStarted = false;
+
+
+
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if(Input.GetMouseButtonDown(0) && !gameStarted      ) 
+        {
+            StartSpawning();
+
+            gameStarted = true;
+        
+        
+        }
+
+
+
+
+
+
+    }
+
+
+
+    private void StartSpawning()
+    {
+        InvokeRepeating("SpawnBlock", 0.5f, spawnRate);
+    }
+
+
+
+
+
+
+
+
+
+
+    private void SpawnBlock()
+    {
+        Vector3 spawnPos = spawnPoint.position;
+
+        spawnPos.x = Random.Range(-maxX, maxX);
+
+        Instantiate(block, spawnPos, Quaternion.identity);
+
+
+
+    }
+
+
+
+
+
+
+}
